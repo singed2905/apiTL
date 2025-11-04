57a0bbe1eb63a39181cb8d715b56da2dd36b034c
Đặng Vũ Hưng <88934176+singed2905@users.noreply.github.com>
2025-11-04 08:55:05 +0700

    Add main Spring Boot application class

---
+++ src/main/java/com/keylog/converter/ConvertKeylogApplication.java
@@ -0,0 +1,11 @@
+package com.keylog.converter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConvertKeylogApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConvertKeylogApplication.class, args);
+    }
+}
