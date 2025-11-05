6601dadd0df8e9e744ac46b97049a7e1afe58d05
Đặng Vũ Hưng <88934176+singed2905@users.noreply.github.com>
2025-11-05 14:10:24 +0700

    revert: remove visualization expose/patch files

--- examples/assets/js/visualization_expose_update.js
+++
@@ -1,6 +0,0 @@
-// Ensure updateVisualization is globally accessible before examples.js/app.js call it
-(function ensureUpdateVisualizationGlobal(){
-  if (typeof window.updateVisualization !== 'function' && typeof updateVisualization === 'function') {
-    window.updateVisualization = updateVisualization;
-  }
-})();
